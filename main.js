9b65081887d9c6fc8a95cccd4a08a558a4f6c26e
Walter Satriano <walter.satri@gmail.com>
2019-08-01 12:16:38 +0200

    refactoring: completata chiamata ajax + generalizzazione contenuti

--- main.js
+++ main.js
@@ -12,16 +12,26 @@ $(document).ready(function() {
   var source = $("#template").html();
   var template = Handlebars.compile(source);
 
+  var dataBase = moment("2018-01-01");
+  var anno = dataBase.format("YYYY");
+  console.log("questo è anno ", anno);
+  var mese = parseInt(dataBase.format("M")) - 1;
+  console.log("questo è mese " , mese);
 
-  //controllo quanti giorni ha il mese (gennaio)
-  var giorniGennaio = moment("2018-01").daysInMonth();
-  console.log(giorniGennaio);
 
-  for (var i = 1; i <= giorniGennaio; i++) {
-    var jan = moment("2018-01").day(i).format("dddd D MMMM");
-    console.log("questo è jan" , jan);
+  //controllo quanti giorni ha il mese
+  var giorniMese = dataBase.daysInMonth();
+  console.log(giorniMese);
 
-    var context = {giorno: jan};
+
+  $(".calendario h1").text(dataBase.format("MMMM YYYY"));
+
+  for (var i = 1; i <= giorniMese; i++) {
+    var jan = moment([anno, mese, i]).format("dddd D MMMM");
+    // console.log("siamo nel day" , moment("2018-01").day(i));
+    // console.log("questo è jan" , jan);
+
+    var context = {giorno: jan, holidays: moment([anno, mese, i]).format("YYYY-MM-DD")};
     var html = template(context);
 
     //stampo a schermo la lista dei giorni
@@ -30,15 +40,30 @@ $(document).ready(function() {
 
 
 
-  var urlHoliday = "https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0";
+
+  // chiamata ajax
+  var urlHoliday = "https://flynn.boolean.careers/exercises/api/holidays";
 
   $.ajax(
     {
       url : urlHoliday,
+      data : {"year": "2018" , "month": mese},
       method : "GET",
       success : function(data) {
         var festa = data.response;
         console.log(festa);
+
+        for (var i = 0; i < festa.length; i++) {
+          var objfesta = festa[i];
+          console.log(objfesta.name, objfesta.date);
+
+          var liFest = $("li[dateref='" + objfesta.date + "']");
+          if (liFest) {
+            liFest.append(", " + objfesta.name);
+            liFest.addClass("red");
+          }
+        }
+
 
       },
       error : function (richiesta,stato,errore) {
